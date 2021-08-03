25c2135a1145e731c04ff763e1337eb66bcfa195
jsprofessional <jshuman@protonmail.com>
2021-08-03 13:57:07 -0500

    Improved UX on iterators.rb

--- iterators.rb
+++ iterators.rb
@@ -1,26 +1,22 @@
 # .any? { |obj| block } => true or false
-puts ["ant", "bear", "cat"].any? { |word| word.length >= 3 }
+puts ".any puts: #{["ant", "bear", "cat"].any? { |word| word.length >= 3 }}"
 
 # .each => calls block once for each element in ruby self, passing that element as a block parameter.
-puts ["ant", "bear", "cat"].each { |word| print word, "--" }
+puts ".each puts: #{["ant", "bear", "cat"].each { |word| print word, "--" }}"
 
 # .collect { |obj| block } => returns a new array with the results of running block once for every element in enum
-puts (1..4).collect { |i| i*i } # => [1, 4, 9, 16]
-puts (1..4).collect { "cat" } # => ["cat", "cat", "cat", "cat"]
-
+puts ".collect #{(1..4).collect { |i| i*i }}"
+puts ".collect #{(1..4).collect { "cat" }}"
 
 # .detect/.find => returns the first for which block is not false.
-puts (1..10).detect { |i| i %5 == 0 and i % 7 == 0 } # => nil
-puts (1..100).detect { |i| i %5 == 0 and i % 7 == 0 } # => 35
-
+puts ".detect puts: #{(1..10).detect { |i| i %5 == 0 and i % 7 == 0 }}"
+puts ".find puts: #{(1..100).find { |i| i %5 == 0 and i % 7 == 0 }}"
 
 # .find_all { |obj| block } or .select { |obj| block } => returns an array containing all elements of enum for which block is not false
-puts (1..10).find_all { |i| i % 3 == 0 } # => [3, 6, 9]
-
+puts ".find_all puts: #{(1..10).find_all { |i| i % 3 == 0 }}"
 
 # .reject { |obj| block } => opposite of find_all
-puts (1..10).reject { |i| i % 3 == 0 } # => [1, 2, 4, 5, 7, 8, 10]
-
+puts ".reject puts: #{(1..10).reject { |i| i % 3 == 0 }}"
 
 # .upto(limit) => iterates block up to the int number
-puts 5.upto(10) { |i| print i, " " } # => 5 6 7 8 9 10
+puts ".upto puts: #{5.upto(10) { |i| print i, " " }}"
