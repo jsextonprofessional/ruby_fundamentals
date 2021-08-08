fa0c51e9aad1563b24726b32a91075b9c06a8e50
Jacob Sexton <jshuman@protonmail.com>
2021-08-08 16:57:03 +0000

    Finished names.rb

---
+++ names.rb
@@ -0,0 +1,16 @@
+# You have 5 names in the 'names' array
+
+def name_spitter
+a = {first_name: "Michael", last_name: "Choi"}
+b = {first_name: "John", last_name: "Doe"}
+c = {first_name: "Jane", last_name: "Doe"}
+d = {first_name: "James", last_name: "Smith"}
+e = {first_name: "Jennifer", last_name: "Smith"}
+names = [a, b, c, d, e]
+puts "You have #{names.length} names in the names array"
+    for i in names
+        puts "The name is '#{i[:first_name]} #{i[:last_name]}'"
+    end
+end
+name_spitter
+
